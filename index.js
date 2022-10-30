b87d92195ff1e96540e3a5ed4faa7f3c54c4530b
teemo <teemo@DESKTOP-JL25QLR>
2022-10-30 18:57:11 -0300

    delete php archive

--- index.js
+++ index.js
@@ -11,13 +11,28 @@
     
  
 */
-  
+$("#Bin").keypress(function (e) {
+    if (e.which != 1 && e.which != 0 && (e.which > 49 || e.which < 48)) {
+        $("#error").html("Only 1 or 0").stop().show().fadeOut("slow");
+      return false;
+   }
+  });
+  $("#Dec").keypress(function (e) {
+    if (e.which != 1 && e.which != 0 && (e.which > 49 || e.which < 48)){
+        $("#error").html("Only 1 or 0").stop().show().fadeOut("slow");
+     return false;
+   }
+  });
+
 $("#calcBtn").on("click", ()=>{
    
+    
+ 
+    
     let bin = $("#Bin").val();   
   
 
-
+    
     $("#Dec").val(binaryToDecimal(bin));
 
    
